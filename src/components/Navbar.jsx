a84c0b5bef77d006d12433a697f5d7ec1ff11204
Antoniyo123 <riswantonr2015@gmail.com>
2026-02-13 19:34:04 +0700

    trial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
         <li><a href="#home">HOME</a></li>
         <li><a href="#about">ABOUT</a></li>
         <li><a href="#portfolio">PORTFOLIO</a></li>
-        <li><a href="#services">SERVICES</a></li>
+        {/* <li><a href="#services">SERVICES</a></li> */}
         <li><a href="#contact">CONTACT US</a></li>
       </ul>
     </nav>
